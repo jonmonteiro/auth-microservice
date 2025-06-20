895298378f7158a43aed23060a36422960d4d5fc
Monteiro712 <jonathanmonteiroa@gmail.com>
2025-06-20 18:55:57 -0300

    refactor

--- src/main/java/auth/domain/product/ProductRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package auth.domain.product;
-
-
-
-public record ProductRequestDTO(
-        String name,
-        Integer price
-) {
-}
--- src/main/java/auth/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import auth.domain.product.Product;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
